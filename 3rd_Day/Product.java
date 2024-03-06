867385fc2420b51667a40fc4da28b63f342b9b4e
Pritam Kumar <49643430+pritamkumar143@users.noreply.github.com>
2024-03-07 02:44:27 +0530

    Update Product.java
    product

--- 3rd_Day/Product.java
+++ 3rd_Day/Product.java
@@ -1,6 +1,6 @@
 import java.util.*;
 // taking input from user
-public class Addinput{
+public class Product{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
