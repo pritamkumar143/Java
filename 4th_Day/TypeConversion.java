7b69b460f50bd51631adbe77b669aa10ceeb912a
Pritam Kumar <pritamkunar13@gmail.com>
2024-03-07 03:00:02 +0530

    Type Conversion

---
+++ 4th_Day/TypeConversion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class TypeConversion {
+    public static void main(String[] args){
+        // int a=24;
+        // long b= a;
+        // System.out.println(b);
+
+        Scanner sc  = new Scanner(System.in);
+        float number = sc.nextFloat();
+        System.out.println(number);
+    }
+}
