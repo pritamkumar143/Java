edd0f79c6891cc4ef32829b3bf67a57b0e507171
Pritam Kumar <pritamkunar13@gmail.com>
2024-03-07 18:12:26 +0530

    Relational operator

---
+++ 6th_day/Relational_Operators/Relational.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class relational {
+    public static void main(String[] args) {
+//Equal
+System.out.println("Equal\n");
+        int A = 10;
+        int B = 10;
+        System.out.println(A == B);
+
+
+// not Equal
+System.out.println("Not Equal\n");
+        int P = 10;
+        int S = 14;
+        System.out.println(P != S);
+        System.out.println("");
+
+
+// Greater than
+
+System.out.println("Greater than\n");
+
+System.out.println(B>A);
+System.out.println("");
+
+//Less than
+System.out.println("Less than\n");
+
+System.out.println(B<A);
+System.out.println("");
+
+
+// Greater than or equal
+
+System.out.println("Greater than or equal\n");
+
+System.out.println( A <= B);
+System.out.println("");
+
+System.out.println("Less than or equal\n");
+System.out.println("");
+
+System.out.println( A >= B);
+
+        
+
+    }
+}
