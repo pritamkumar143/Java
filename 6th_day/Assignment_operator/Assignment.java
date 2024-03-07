0dc45841f49016d98d558b5f3906187048375905
Pritam Kumar <pritamkunar13@gmail.com>
2024-03-07 18:30:01 +0530

    Assignment operator

---
+++ 6th_day/Assignment_operator/Assignment.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Assignment {
+    public  static  void main(String[] args) {
+         int  a = 12;
+        //  a = a + 10;
+         a+=10;
+         int b = 34;
+        //  b = b-10;
+         b -= 10;
+
+         System.out.println(a);
+         System.out.println(b);
+    }
+}
