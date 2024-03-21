002ee198e9e1a1c25017904ca13b2d2afeceaa44
Pritam Kumar <pritamkunar13@gmail.com>
2024-03-21 17:25:53 +0530

    while loop

---
+++ 8th_Day/Whileloop.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class whileloop {
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        while (count <20){
+        
+        
+        System.out.println("1 crore in 2025");
+        count++;
+        }
+    }
+}
