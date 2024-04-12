2e08bdc3ca85874c117f0af4de5eb60feb5b83d5
Pritam Kumar <pritamkunar13@gmail.com>
2024-04-13 00:10:14 +0530

    binary Search s

---
+++ 13th_Day/reverse_array.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class reverseArray {
+
+    public  static void reverse(int numbers[]){
+        int start=0, end= numbers.length -1;
+
+        while(start < end) {
+
+            //Swap
+            
+            int temp = numbers[end];
+            numbers[end] =numbers[start];
+            numbers[start] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10};
+        //print
+        reverse(numbers);
+        for(int i=0; i<numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+       }
+       System.out.println("");
+    }
